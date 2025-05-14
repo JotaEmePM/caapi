0a0f69ba02236c11e2947f5b50c97593d3e426e7
Jose Miguel Perez <jose.perez@mapfre.cl>
2025-05-14 17:16:35 -0400

    feat: cat module impl

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    load: [configuration]
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration]
+    }),
+    MongooseModule.forRoot(`${process.env.MONGO_URI}`)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './cats.interfaces';
+import { Cat } from 'src/schemas/cat.schema';
+
+
+@Controller()
+export class CatsController {
+    constructor(private readonly catService: CatsService) { }
+
+    @Get()
+    async getAllCats(): Promise<string[]> {
+        let cats = await this.catService.findAll();
+        return cats.map(cat => cat.name);
+    }
+
+    @Post()
+    async createCat(@Body() createCatDto: CreateCatDto): Promise<Cat> {
+        return this.catService.create(createCatDto);
+    }
+}
