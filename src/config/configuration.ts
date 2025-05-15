8c105a827a99bce3ec649971854b4203a9b6b8ac
Jose Miguel Perez <jose.perez@mapfre.cl>
2025-05-15 15:48:02 -0400

    Test: MongoConnection2

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,8 +1,12 @@
+import { Logger } from "@nestjs/common"
+
 export default () => ({
     environment: process.env.APP_ENV || 'development',
 })
 
 export const getMongoConnectionString = (): string => {
-    return `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_USERNAME}@${process.env.MONGO_URL}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+    const cnnstr = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_USERNAME}@${process.env.MONGO_URL}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+    Logger.log(`MongoDBConnectionString: ${cnnstr}`, 'MongoDB Connection String')
+    return cnnstr
 }
 
