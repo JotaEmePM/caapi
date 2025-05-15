ae3bfae7bb76609b9b6f4a58a6e401579978ca9d
Jose Miguel Perez <jose.perez@mapfre.cl>
2025-05-15 15:35:05 -0400

    Test: MongoConnection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { getMongoConnectionString } from './config/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  Logger.log(`MongoDBConnectionString: ${getMongoConnectionString()}`, 'MongoDB Connection String');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
